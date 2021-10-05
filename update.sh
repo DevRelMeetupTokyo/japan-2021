15257c1a7206327db4d5ca53d7ca34c9d13c0fd1
ubuntu <ubuntu@ik1-420-42354.vs.sakura.ne.jp>
2021-10-05 16:55:45 +0900

    Auto update

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+source /home/ubuntu/.bashrc
 nvm use v16.10.0
 
 git pull
@@ -7,5 +8,7 @@ sh getData.sh
 git add .
 git commit -m "Auto update"
 git push
+
+docker run --rm --volume="$PWD:/srv/jekyll" jekyll/jekyll jekyll build
 
 npm run deploy
