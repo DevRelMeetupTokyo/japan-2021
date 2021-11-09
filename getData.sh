8281cc6141b5e8485eed5eb61860f99a283377ab
Atsushi <atsushi@moongift.jp>
2021-11-09 11:05:16 +0900

    Updates

--- getData.sh
+++ getData.sh
@@ -3,6 +3,8 @@ wget https://script.google.com/macros/s/AKfycbw9-dG6XuILpo9xcvPcjw8pdqLt3BXo9_4n
 wget https://script.google.com/macros/s/AKfycbw9-dG6XuILpo9xcvPcjw8pdqLt3BXo9_4nxJl8eKHW0uOpuq7Iyy8HNS_J_xu6bjPH/exec?name=sponsors -O _data/sponsors.json 
 wget https://script.google.com/macros/s/AKfycbw9-dG6XuILpo9xcvPcjw8pdqLt3BXo9_4nxJl8eKHW0uOpuq7Iyy8HNS_J_xu6bjPH/exec?name=speakers -O _data/speakers.json 
 wget https://script.google.com/macros/s/AKfycbw9-dG6XuILpo9xcvPcjw8pdqLt3BXo9_4nxJl8eKHW0uOpuq7Iyy8HNS_J_xu6bjPH/exec?name=sessions -O _data/sessions.json 
+wget https://script.google.com/macros/s/AKfycbw9-dG6XuILpo9xcvPcjw8pdqLt3BXo9_4nxJl8eKHW0uOpuq7Iyy8HNS_J_xu6bjPH/exec?name=tracks -O _data/tracks.json 
 
-ruby speaker.rb
+ruby organizer.rb
 ruby sessions.rb
+ruby speaker.rb
